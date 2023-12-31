8c4eea04b7bbd32fe9de9dbbd086c97dce41bf35
Andre Lew <118043566+aelew@users.noreply.github.com>
2023-12-31 14:12:36 -0800

    improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
-    import Select from '$lib/Select.svelte';
+	import Select from '$lib/Select.svelte';
+	import { get, writable } from 'svelte/store';
 
-    let options = {
-        food:['Cookies', 'Pizza', 'Burgers']
-    }
+	let options = {
+		food: ['Cookies', 'Pizza', 'Burgers']
+	};
+
+	const selected = writable({ label: 'Cookies', value: 'Cookies' });
+
+	function handleSubmit() {
+		console.log(get(selected));
+	}
 </script>
 
-<div class="w-fit">
-    <Select title="Food" placeholder="Select your Food" options={options} />
-</div>
-
-
+<form class="w-fit" on:submit={handleSubmit}>
+	<Select title="Food" placeholder="Select your Food" {selected} {options} />
+	<button class="inline-flex items-center justify-center shadow text-sm px-4 py-2 font-semibold bg-glacier-500 rounded-lg hover:bg-glacier-500/80 transition-colors">Submit</button>
+</form>
