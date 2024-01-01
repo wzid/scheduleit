21c779d4f43198d382a4aea9fdedfac51efc8f80
cameron <me@cameron.rs>
2024-01-01 00:44:07 -0500

    update header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,5 @@
 <!-- Not sure what to add here until we get further down in development-->
 <div class="pb-8">
-    <h1 class="leading-none">timeslot</h1>
+    <h1 class="w-fit leading-none text-transparent bg-clip-text bg-gradient-to-r from-royalblue-200 to-royalblue-300">timeslot</h1>
     <p class="text-sm text-zinc-500">built by cameron and andre</p>
 </div>
