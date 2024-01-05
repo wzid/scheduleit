0c0ae7f859662828a19c8b380a5290490dffaa83
aelew <118043566+aelew@users.noreply.github.com>
2024-01-04 21:35:20 -0800

    minor changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,9 @@
 <script lang="ts">
-	import { Select, Button, Input, Calendar, DateRangePicker }  from '$lib';
+	import { Select, Button, Input, Calendar, DateRangePicker } from '$lib';
+	import { TIMES } from '$lib/constants';
 	import { get, writable } from 'svelte/store';
 
-	let options = {
-		times: [
-			'12:00 AM',
-			'12:30 AM',
-			'1:00 AM',
-			'1:30 AM',
-			'2:00 AM',
-			'2:30 AM',
-			'3:00 AM',
-			'3:30 AM',
-			'4:00 AM',
-			'4:30 AM',
-			'5:00 AM',
-			'5:30 AM',
-			'6:00 AM',
-			'6:30 AM',
-			'7:00 AM',
-			'7:30 AM',
-			'8:00 AM',
-			'8:30 AM',
-			'9:00 AM',
-			'9:30 AM',
-			'10:00 AM',
-			'10:30 AM',
-			'11:00 AM',
-			'11:30 AM',
-			'12:00 PM',
-			'12:30 PM',
-			'1:00 PM',
-			'1:30 PM',
-			'2:00 PM',
-			'2:30 PM',
-			'3:00 PM',
-			'3:30 PM',
-			'4:00 PM',
-			'4:30 PM',
-			'5:00 PM',
-			'5:30 PM',
-			'6:00 PM',
-			'6:30 PM',
-			'7:00 PM',
-			'7:30 PM',
-			'8:00 PM',
-			'8:30 PM',
-			'9:00 PM',
-			'9:30 PM',
-			'10:00 PM',
-			'10:30 PM',
-			'11:00 PM',
-			'11:30 PM'
-		]
-	};
+	let options = { times: TIMES };
 
 	// These are the default values of the select component
 	const fromTime = writable({ label: '12:00 AM', value: '12:00 AM' });
@@ -68,7 +18,7 @@
 </script>
 
 <form class="w-fit flex flex-col gap-4" on:submit={handleSubmit}>
-	<Input bind:value={eventName} size="lg" placeholder="Type your event name here"/>
+	<Input bind:value={eventName} size="lg" placeholder="Type your event name here..." />
 	<div class="flex gap-4">
 		<div>
 			<div class="pb-2">
