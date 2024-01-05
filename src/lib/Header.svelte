eaea15655234a942553b091107d5a468a2dea52b
cameron <me@cameron.rs>
2024-01-05 12:12:07 -0500

    hover effects

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,56 @@
 <!-- Not sure what to add here until we get further down in development-->
 <div class="pb-8">
-    <h1 class="w-fit leading-none text-transparent bg-clip-text bg-gradient-to-r from-peach-200 to-peach-300">timeslot.one</h1>
-    <p class="text-sm text-zinc-500">built by cameron and andre</p>
+	<h1
+		class="w-fit leading-none text-transparent bg-clip-text bg-gradient-to-r from-peach-200 to-peach-300"
+	>
+		timeslot.one
+	</h1>
+	<div class="text-center text-sm text-zinc-500">
+		built by <a class="playful" target="_blank" href="https://github.com/wzid">cameron</a> and <a class="shine" target="_blank" href="https://github.com/aelew">andre</a>
+	</div>
 </div>
+
+<style lang="postcss">
+	:root {
+		--shadow: #fd793a;
+        --text-color: #71717a
+	}
+
+    .playful {
+        transition: all .25s ease-in-out;
+    }
+
+	.playful:hover {
+        font-weight: bold;
+        color: #fff;
+		text-shadow:
+			1.75px 1.75px var(--shadow),
+			1.5px 1.5px var(--shadow),
+			1.25px 1.25px var(--shadow),
+			1px 1px var(--shadow),
+			0.75px 0.75px var(--shadow),
+			0.5px 0.5px var(--shadow),
+			0.25px 0.25px var(--shadow);
+	}
+
+    .shine {
+        background-color: #71717a;
+        background-image: linear-gradient(
+            -75deg,
+            var(--text-color) 0,
+            var(--text-color) 5%,
+            rgba(255, 255, 255, 0.5) 5%,
+            rgba(255, 255, 255, 0.5) 25%,
+            var(--text-color) 15%,
+            var(--text-color) 100%
+        );
+        background-size: 200% 100%;
+        -webkit-text-fill-color: transparent;
+        -webkit-background-clip: text;
+        transition: all .5s;
+    }
+
+    .shine:hover {
+        background-position: -150% 0;
+    }
+</style>
