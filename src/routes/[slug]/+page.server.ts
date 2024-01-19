9a5cd2c8a55e3a2fdad2c6deba67033a04be8a4d
cameron <me@cameron.rs>
2024-01-19 00:38:04 -0500

    Fix 404 page, start on users

--- src/routes/[slug]/+page.server.ts
+++ src/routes/[slug]/+page.server.ts
@@ -1,12 +1,16 @@
 import { error } from '@sveltejs/kit';
 import { db } from '$lib/db';
-import { events } from '$lib/db/schema.js';
+import { events, users } from '$lib/db/schema.js';
 import { eq } from 'drizzle-orm';
 
 export async function load({ params }) {
   const result = await db.select().from(events).where(eq(events.id, params.slug));
-  if (!result) {
+  
+  if (!result || result.length === 0 || !result[0]) {
     throw error(404, `Could not find event with id ${params.slug}`);
   }
-  return { event: result[0] };
+
+  const eventUsers = await db.select().from(users).where(eq(users.eventId, params.slug));
+
+  return { event: result[0], users: eventUsers };
 }
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -2,11 +2,13 @@
   import { ClipboardCopy, Check } from 'lucide-svelte';
   import { writable } from 'svelte/store';
   import { fly } from 'svelte/transition';
+  import { error } from '@sveltejs/kit';
 
   import { Button, Meta } from '$lib';
 
   export let data;
   const event = data.event;
+  const users = data.users;
 
   // create a store to track the tooltip state
   const open = writable(false);
@@ -20,29 +22,33 @@
   };
 </script>
 
-{#if event}
-  <Meta title={event.name} />
+<Meta title={event.name} />
 
-  <h1>{event.name}</h1>
+<h1>{event.name}</h1>
 
-  <div class="mt-4 w-fit">
-    <Button onClick={copyLink} className="text-base font-medium" variant="neutral">
-      <ClipboardCopy class="mr-2 h-5 w-5" />
-      Copy Link
-    </Button>
-    {#if $open}
-      <div
-        transition:fly={{ duration: 200, y: -10 }}
-        class="-z-10 mx-auto w-fit rounded-lg bg-zinc-700/70 shadow-lg"
-      >
-        <div class="mt-3 flex items-center px-3 py-1 text-zinc-300">
-          <span>Copied</span>
-          <Check class="ml-2 h-4 w-4" strokeWidth={3} />
-        </div>
+<div class="mt-4 w-fit">
+  <Button onClick={copyLink} className="text-base font-medium" variant="neutral">
+    <ClipboardCopy class="mr-2 h-5 w-5" />
+    Copy Link
+  </Button>
+  {#if $open}
+    <div
+      transition:fly={{ duration: 200, y: -10 }}
+      class="-z-10 mx-auto w-fit rounded-lg bg-zinc-700/70 shadow-lg"
+    >
+      <div class="mt-3 flex items-center px-3 py-1 text-zinc-300">
+        <span>Copied</span>
+        <Check class="ml-2 h-4 w-4" strokeWidth={3} />
       </div>
-    {/if}
+    </div>
+  {/if}
+</div>
+
+<div class="mt-8">
+  <div>
+    <h2>Respondents</h2>
+    {#each users as user}
+      <p>{user.name}</p>
+    {/each}
   </div>
-{:else}
-  <h1>404</h1>
-  <p>Event not found</p>
-{/if}
+</div>
