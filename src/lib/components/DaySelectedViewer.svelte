3bcf3141a4586d9c87d3be5b8522a898c0535e3b
aelew <118043566+aelew@users.noreply.github.com>
2024-01-23 00:18:58 -0800

    add available list tooltip

--- src/lib/components/DaySelectedViewer.svelte
+++ src/lib/components/DaySelectedViewer.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { DAYS_OF_THE_WEEK, type Day } from '$lib/constants';
+  import { DAYS_OF_THE_WEEK } from '$lib/constants';
   import { cn } from '$lib/utils';
   import type { User } from '$lib/constants';
 
@@ -20,11 +20,28 @@
   </div>
   <div class="flex justify-between gap-1">
     {#each DAYS_OF_THE_WEEK as day, i}
-      <!-- svelte-ignore a11y-no-static-element-interactions a11y-mouse-events-have-key-events -->
-      <div
-        style="background-color: {shades[daysSelected.get(day)?.length ?? 0]};"
-        class={cn('h-10 w-10', boxClassNames[day])}
-      ></div>
+      <div class="group relative">
+        <div
+          style="background-color: {shades[daysSelected.get(day)?.length ?? 0]};"
+          class={cn('h-10 w-10', boxClassNames[day])}
+        />
+        <div
+          class="pointer-events-none absolute left-1/2 top-12 z-10 hidden -translate-x-1/2 whitespace-nowrap group-hover:block"
+        >
+          <div
+            class="animate-in fade-in slide-in-from-bottom-2 rounded-lg bg-zinc-700 px-2 py-1 text-sm text-zinc-300 shadow-lg"
+          >
+            <strong>
+              Available: <span class="font-normal">{daysSelected.get(day)?.length ?? 0}</span>
+            </strong>
+            <ul class="list-inside list-disc pl-1">
+              {#each daysSelected.get(day)?.map((user) => user.name) ?? [] as availableUserName}
+                <li>{availableUserName}</li>
+              {/each}
+            </ul>
+          </div>
+        </div>
+      </div>
     {/each}
   </div>
 </div>
