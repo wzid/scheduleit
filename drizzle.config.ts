46541f7c75c75118f1520723f08b5e7b5974c10e
Andre Lew <118043566+aelew@users.noreply.github.com>
2025-03-31 10:34:20 -0700

    fix drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,9 @@ import 'dotenv/config';
 export default defineConfig({
   schema: './src/lib/db/schema.ts',
   out: './drizzle',
-  driver: 'turso',
+  dialect: 'turso',
   dbCredentials: {
-    url: process.env.TURSO_DB_URL as string,
-    authToken: process.env.TURSO_DB_AUTH_TOKEN as string
+    url: process.env.TURSO_DB_URL!,
+    authToken: process.env.TURSO_DB_AUTH_TOKEN!
   }
 });
