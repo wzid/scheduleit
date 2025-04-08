a0f2adc6ee3e5c154444bf1256354e9d7d7ea92a
Andre Lew <me@aelew.com>
2025-04-07 20:38:02 -0700

    update shading

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -144,7 +144,7 @@ export function shadeGradient(shades: number) {
   return levels.map((x) => {
     // this is the background color of the selector
     if (x === 0) return '#33333a';
-    if (x < 0.33) return lerpColor('#ffe8d5', '#ffa872', progress(0, 0.33, x));
+    if (x < 0.33) return lerpColor('#ecd8c7', '#ffa872', progress(0, 0.33, x));
     if (x < 0.69) return lerpColor('#ffa872', '#fd793a', progress(0.33, 0.69, x));
     if (x < 0.82) return lerpColor('#fd793a', '#ea4c20', progress(0.69, 0.82, x));
     return lerpColor('#ea4c20', '#c4290a', progress(0.82, 1, x));
